8634f8e5ba8a20e35366388266d8e8557477e77d
Vandal2006 <henryjgilson@lewisu.edu>
2022-06-04 14:24:06 -0500

    update mania lite should show on main page

--- static/map-mania-lite/map-mania-lite.js
+++ static/map-mania-lite/map-mania-lite.js
@@ -1,9 +1,18 @@
 
 var gMap;
 
+
+
 function initMap() {
     gMap = new google.maps.Map(document.getElementById("myMapID"), {
-      center: { lat: 41.878, lng: 10 }, zoom: 3,});
+      center: { lat: 41.878, lng: 10 }, zoom: 3});
+
+     //adding roatan marker, var marker dosnt work?
+     
+    var marker = new google.maps.Marker({postion:{lat:16.3298,lng:-86.5300}, map: gMap});
+
+    
+     
   }
   
 
