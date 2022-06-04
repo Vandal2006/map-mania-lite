15053b89d0727498323dc308d3248d2166d31b1d
Vandal2006 <henryjgilson@lewisu.edu>
2022-06-04 18:26:02 -0500

    got the marker to work, not sure how

--- static/map-mania-lite/map-mania-lite.js
+++ static/map-mania-lite/map-mania-lite.js
@@ -8,16 +8,7 @@ function initMap() {
       center: { lat: 41.878, lng: 10 }, zoom: 3});
 
      //adding roatan marker, var marker dosnt work?
-     
-    var marker = new google.maps.Marker({postion:{lat:16.3298,lng:-86.5300}, map: gMap});
-
+    var marker = new google.maps.Marker({position:{lat:16.3298,lng:-86.5300}, map:gMap});
     
      
   }
-  
-
-
-
-function initApplication(){
-    console.log('map mania lite - starting!')
-}
