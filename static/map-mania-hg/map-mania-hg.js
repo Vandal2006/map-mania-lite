f1011a5fb9b951bf2fba1d52d0fea4421a43d070
Vandal2006 <henryjgilson@lewisu.edu>
2022-06-04 19:27:14 -0500

    added the actual map to map mania

--- static/map-mania-hg/map-mania-hg.js
+++ static/map-mania-hg/map-mania-hg.js
@@ -1,1 +1,10 @@
 console.log('Map Mania js')
+
+var gMap;
+
+
+
+function initMap() {
+    gMap = new google.maps.Map(document.getElementById("myMapID"), {
+      center: { lat: 41.878, lng: 10 }, zoom: 3});
+    }
